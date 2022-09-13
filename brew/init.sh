5d8569b54e38519ad9007943ae0290d15b6d1478
Floris van der Grinten <floris@grinten.com>
2022-09-13 13:34:35 +0200

    Add brew to PATH

--- brew/init.sh
+++ brew/init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# shellcheck source=../lib.sh
+# shellcheck source=../lib.sh disable=SC2016
 . lib.sh
 
 # Install brew
 if ! command -v brew &>/dev/null; then
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  if command -v /opt/homebrew/bin/brew &>/dev/null; then
+    rcadd "$(/opt/homebrew/bin/brew shellenv)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
 fi
 
 # Install everything in Brewfile
